93c14decf2ff37d350f49d418d03dff8d9e611b2
26-g <guiller.nike26@gmail.com>
2020-09-24 17:28:52 -0500

    primer commit

---
+++ Datos/PersonaRepository.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Datos
+{ 
+    public class PersonaRepository 
+    {
+        private readonly Sq1Connection _connection;
+        private readonly List<Persona> _personas = new List<Persona>();
+
+        public PersonaRepository(ConnectionManager connection) 
+{
+            _connection = connection. conexion; 
+        }
+                public void Guardar(Persona persona)  {
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = @"Insert Into Persona (Identificacion,Nombre,Edad, Sexo, Pulsacion) 
+                                        values (@Identificacion,@Nombre,@Edad,@Sexo,@Pulsacion)";
+                command.Parameters.AddWithValue("@Identificacion", persona.Identificacion);
+                command.Parameters.AddWithValue("@Nombre", persona.Nombre);
+                command.Parameters.AddWithValue("@Sexo", persona.Sexo);
+                command.Parameters.AddWithValue("@Edad", persona.Edad);
+                command.Parameters.AddWithValue("@Pulsacion", persona.Pulsacion);
+                var filas = command.ExecuteNonQuery();
+            }
+        }
+
+        
+    
+               public void Eliminar(Persona persona) {
+                Personas.Remove(persona);
+         }
+
+        public List<Persona> ConsultarTodos()
+        {
+            SqlDataReader dataReader;
+            List<Persona> personas = new List<Persona>();
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = "Select * from persona ";
+                dataReader = command.ExecuteReader();
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        Persona persona = DataReaderMapToPerson(dataReader);
+                        personas.Add(persona);
+                    }
+                }
+            }
+            return personas;
+        }
+
+        public Persona BuscarPorIdentificacion(string identificacion)
+        {
+            SqlDataReader dataReader;
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = "select * from persona where Identificacion=@Identificacion";
+                command.Parameters.AddWithValue("@Identificacion", identificacion);
+                dataReader = command.ExecuteReader();
+                dataReader.Read();
+                return DataReaderMapToPerson(dataReader);
+            }
+        }
+
+        private Persona DataReaderMapToPerson(SqlDataReader dataReader){
+            if(!dataReader.HasRows) return null;
+            Persona persona = new Persona();
+            persona.Identificacion = (string)dataReader["Identificacion"];
+            persona.Nombre = (string)dataReader["Nombre"];
+            persona.Sexo = (string)dataReader["Sexo"];
+            persona.Edad = (int)dataReader["Edad"];
+            return persona;
+
+              public GuardarPersonaResponse Guardar(Persona persona)
+        {
+            try
+            {
+                persona.CalcularPulsaciones();
+                _conexion.Open();
+                _repositorio.Guardar(persona);
+                _conexion.Close();
+                return new GuardarPersonaResponse(persona);
+            }
+            catch (Exception e)
+            {
+                return new GuardarPersonaResponse($"Error de la Aplicacion: {e.Message}");
+            }
+            finally { _conexion.Close(); }
+        }
+
+}
+
+        public int TotalizarMujeres();
+
+        public int TotalizarHombres();
+    }
+}
---
+++ Logica/PersonaService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Logica
+  public class PersonaService
+    {
+        PersonaRepository PersonaRepositorio   = new PersonaRepository();
+        public String Guardar(Persona persona)
+        {
+            if (PersonaRepositorio.BuscarIdentificacion(persona.Identificacion)==null)
+            {
+                PersonaRepositorio.Guardar(persona);
+                return $"se guardo la persona con la identificacion {persona.Identificacion}";
+            }
+            return $"la persona con identificacion {persona.Identificacion} no se guardo";
+    
+
+        }
+
+        public String Eliminar(Persona persona)
+        {
+            if (persona== null)
+            {
+                return $"la persona {persona.Identificacion} no se encuentra registrada ";
+            }
+            if (PersonaRepositorio.BuscarIdentificacion(persona.Identificacion)==null)
+            {
+                return $"la identificacion {persona.Identificacion}la persona no se encuentra registrada ";
+            }
+            PersonaRepositorio.Eliminar(persona);
+            return $"la identificacion {persona.Identificacion}se elimino correctamente";
+        }
+
+        public List<Persona> Consultar()
+        {
+            return PersonaRepositorio.Consultar();
+        }
+        public Persona Buscar(string id)
+
+        {
+            return PersonaRepositorio.BuscarIdentificacion(id);
+        }
+
+
+            public class GuardarPersonaResponse 
+    {
+        public GuardarPersonaResponse(Persona persona)
+        {
+            Error = false;
+            Persona = persona;
+        }
+        public GuardarPersonaResponse(string mensaje)
+        {
+            Error = true;
+            Mensaje = mensaje;
+        }
+        public bool Error { get; set; }
+        public string Mensaje { get; set; }
+        public Persona Persona { get; set; }
+    }
+
+    public Persona BuscarPorIdentificacion(string identificacion)
+
+        {
+            return PersonaRepositorio.BuscarPorIdentificacion(id);
+        }
